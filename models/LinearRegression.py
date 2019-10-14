3a50992ace3f2e65e52d6a40d666a915e1d334b2
dtucunduva <daniel.tucunduva@toptal.com>
2019-10-14 12:55:16 -0400

    Add torch implementations

--- models/LinearRegression.py
+++ models/LinearRegression.py
@@ -1,7 +1,77 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
 from sklearn.linear_model import SGDClassifier
 
+# Pytorch Linear Model Class
+class LinearRegressionModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(3072, 1)  # One in and one out
 
-class LinearRegression:
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+    # Linear Regression Class (using Pytorch function)
+
+
+# epochs (float, optional) – epochs  (default: 500)
+# lr (float, optional) – learning rate (default: 0.001)
+# momentum (float, optional) – momentum factor (default: 0.9)
+# weight_decay (float, optional) – weight decay (L2 penalty) (default: 0)
+# x_scale (float, optional) – Since the maximum number of R,G,B is 255, I devide every X data by 255 to make the value of X at the same scale with Y (default: 255)
+
+class LinearRegression():
+    #    def __init__(self,epochs = 500, lr=0.001,momentum=0.9, weight_decay=0, x_scale=255):
+    def __init__(self, epochs=500, lr=0.001, momentum=0.9, decay=0, x_scale=255):
+        # Hyperparameters
+        self.momentum = momentum
+        self.lr = lr
+        self.epochs = epochs
+        self.decay = decay
+        self.x_scale = x_scale
+
+        # our model
+        self.linear = LinearRegressionModel()
+
+    def train(self, X_train, y_train):
+        # prepare the data for training.
+        X = X_train.copy()
+        X = np.array(X, np.float32) / self.x_scale
+        x_data = Variable(torch.Tensor(X))
+        y_data = Variable(torch.Tensor(y_train))
+
+        criterion = torch.nn.MSELoss(size_average=True)
+        # We also tried other loss function such as L1Loss
+        # criterion = torch.nn.L1Loss()
+
+        #      optimizer = torch.optim.SGD(self.linear.parameters(), lr=self.lr,momentum=self.momentum,weight_decay=self.decay)
+        optimizer = torch.optim.Adagrad(self.linear.parameters(), lr=self.lr, lr_decay=self.decay)
+        for epoch in range(self.epochs):
+            # Forward pass: Compute predicted y by passing
+            # x to the model
+            pred_y = self.linear(x_data)
+
+            # Compute and print loss
+            loss = criterion(pred_y, y_data)
+
+            # Zero gradients, perform a backward pass,
+            # and update the weights.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    def predict(self, X_test):
+        # prepare the data for training.
+        XV = X_test.copy()
+        XV = np.array(XV, np.float32) / self.x_scale
+        new_var = Variable(torch.Tensor(XV))
+        pred = self.linear(new_var)
+        return pred
+
+
+class LinearRegressionSKL:
     def __init__(self):
         """
         Initialises LinearRegression classifier with initializing
@@ -10,14 +80,14 @@ class LinearRegression:
         self.alpha = 0.01
         self.epochs = 1000
         self.linear_model = SGDClassifier(learning_rate='adaptive', eta0=self.alpha,
-                                                max_iter=self.epochs, loss='squared_loss', n_jobs=-1)
+                                          max_iter=self.epochs, loss='squared_loss', n_jobs=-1)
 
     def train(self, X_train, y_train):
         """
         Train Linear regression classifier
         """
         self.linear_model.fit(X=X_train, y=y_train)
-    
+
     def predict(self, X_test):
         """
         Inputs:
--- models/LogisticRegression.py
+++ models/LogisticRegression.py
@@ -1,7 +1,66 @@
+import torch
+from torch.autograd import Variable
 from sklearn.linear_model import SGDClassifier
 
+# Logistic Regression Class (using Pytorch function)
+# epochs (float, optional) – epochs  (default: 500)
+# lr (float, optional) – learning rate (default: 0.001)
+# momentum (float, optional) – momentum factor (default: 0.9)
+# weight_decay (float, optional) – weight decay (L2 penalty) (default: 0)
 
-class LogisticRegression:
+class LogisticRegressionM(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegressionM, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        outputs = self.linear(x)
+        return outputs
+
+
+class LogisticRegression():
+    #    def __init__(self,epochs = 6, lr=0.001,momentum=0.9, weight_decay=0):
+    def __init__(self, epochs=6, lr=0.001, momentum=0.9, decay=0):
+        # Hyperparameters
+        self.momentum = momentum
+        self.lr = lr
+        self.epochs = epochs
+        self.decay = decay
+        self.logistic = LogisticRegressionM(3072, 10)
+
+    def train(self, train_loader):
+        criterion = torch.nn.CrossEntropyLoss()  # computes softmax and then the cross entropy
+        #      optimizer = torch.optim.SGD(self.logistic.parameters(), lr=self.lr,momentum=self.momentum,weight_decay=self.weight_decay)
+        optimizer = torch.optim.Adagrad(self.logistic.parameters(), lr=self.lr, lr_decay=self.decay)
+        for epoch in range(int(self.epochs)):
+            for i, (images, labels) in enumerate(train_loader):
+                images = Variable(images.view(-1, 3 * 32 * 32))
+                labels = Variable(labels)
+
+                optimizer.zero_grad()
+                outputs = self.logistic(images)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+    def predict(self, test_loader):
+        # calculate Accuracy
+        pred = []
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = Variable(images.view(-1, 3 * 32 * 32))
+            outputs = self.logistic(images)
+            _, predicted = torch.max(outputs.data, 1)
+            pred.append(predicted)
+            total += labels.size(0)
+            # for gpu, bring the predicted and labels back to cpu fro python operations to work
+            correct += (predicted == labels).sum()
+        accuracy = 100 * correct / total
+        return {"pred": pred, "accuracy": accuracy}
+
+
+class LogisticRegressionSKL:
     def __init__(self):
         """
         Initialises LogisticRegression classifier
@@ -33,3 +92,5 @@ class LogisticRegression:
         """
         pred = self.logistic_regression.predict(X_test)
         return pred
+
+
